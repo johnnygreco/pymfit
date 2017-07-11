78e5fe52b57f29d891a2b9678b763bcdb279d422
Johnny Greco <jgreco@astro.princeton.edu>
2017-07-11 11:32:31 -0400

    can take array as input

--- pymfit/utils.py
+++ pymfit/utils.py
@@ -1,7 +1,10 @@
 from astropy.io import fits
 
-def get_img_center(img_fn, ext=1):
-    head = fits.getheader(img_fn, ext=ext)
-    X0 = head['NAXIS1']
-    Y0 = head['NAXIS2']
+def get_img_center(img, ext=1):
+    if type(img)==str:
+        head = fits.getheader(img, ext=ext)
+        X0 = head['NAXIS1']
+        Y0 = head['NAXIS2']
+    else:
+        Y0, X0 = img.shape
     return X0/2, Y0/2    
